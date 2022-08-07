c0d3ae4c290d6d0244f04b4f43f32f3cce57910d
joaquinbidegain <joaquininternacional1@gmail.com>
2022-08-07 16:20:38 -0300

    api metodo update

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -5,6 +5,7 @@ var bicicletasController = require('../../controllers/api/bicicletaControllerAPI
 router.get('/', bicicletasController.bicicleta_list);
 router.post('/create', bicicletasController.bicicleta_create);
 router.delete('/delete', bicicletasController.bicicleta_delete);
+router.post('/update', bicicletasController.bicicleta_update);
 
 
 module.exports = router;
